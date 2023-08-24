b464149b5237a4de5ea4c0fbb9c476af2f332725
luan-alencar <luanalencar134@gmail.com>
2023-08-24 18:57:58 -0300

    comentarios - luan

--- src/main/java/com/unifacisa/tap/usuarioservice/service/mapper/ProdutoMapper.java
+++ src/main/java/com/unifacisa/tap/usuarioservice/service/mapper/ProdutoMapper.java
@@ -9,7 +9,11 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface ProdutoMapper {
 
-    // INSTANCE é uma forma de referenciar a função de mapeamento produtoToDTO do nosso ProdutoMapper.
+    /**
+     * INSTANCE é uma forma de referenciar a função de mapeamento produtoToDTO do nosso ProdutoMapper
+     * O código ProdutoMapper INSTANCE cria uma instância do mapeador definido na interface ProdutoMapper.
+     * Essa instância é armazenada na variável INSTANCE para ser usada posteriormente para realizar os mapeamentos.
+     */
     ProdutoMapper INSTANCE = Mappers.getMapper(ProdutoMapper.class);
 
     ProdutoDTO produtoToDTO(Produto produto);
--- src/main/java/com/unifacisa/tap/usuarioservice/service/mapper/UsuarioMapper.java
+++ src/main/java/com/unifacisa/tap/usuarioservice/service/mapper/UsuarioMapper.java
@@ -8,7 +8,12 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
-    // INSTANCE é uma forma de referenciar a função de mapeamento usuarioToDTO do nosso UsuarioMapper.
+
+    /**
+     * INSTANCE é uma forma de referenciar a função de mapeamento usuarioToDTO do nosso UsuarioMapper.
+     * O código ProdutoMapper INSTANCE cria uma instância do mapeador definido na interface ProdutoMapper.
+     * Essa instância é armazenada na variável INSTANCE para ser usada posteriormente para realizar os mapeamentos..
+     */
     UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
 
     UsuarioDTO usuarioToDTO(Usuario usuario);
